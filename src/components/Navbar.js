d1ee003b140b6b203bcdbc409b9405ad06975ae3
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-07 18:21:05 +0530

    removed link from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,10 @@ export default function Navbar(props) {
       <Link className='titleofnav' to = '/'><b>Home</b></Link>
       <span className='boxOfNavbarRight'>
         <Link className='comps' id='log' to='/'>{props.loggedIn ? 'Profile' : 'Links'}</Link>
-        {props.loggedIn ? <Link className='comps' to ='/signOut'>Logout</Link> : <Link className='comps' to ='/login'>Login</Link>}
+        {props.loggedIn ? <Link className='comps' to ='/signout'>Logout</Link> : <Link className='comps' to ='/login'>Login</Link>}
         {/* Currently clicking login changes "login from navbar to logout. In the future this should take us to 
         login page where after we enter details and click login, then only the navbar should change to logout. 
         And add functionality to logout instead of just changing page." */}
-        <Link className='comps' to='/'>Help</Link>
       </span>
     </div>
   )
