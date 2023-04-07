b94436361ccc5500531b6f0cb7a62d5751de4d76
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-07 18:20:48 +0530

    added css file for login signup logout

--- src/components/LoginBody.js
+++ src/components/LoginBody.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/LoginBody.css'
 
 export default function LoginBody() {
   return (
--- src/components/SignUpBody.js
+++ src/components/SignUpBody.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/SignUpBody.css'
 
 export default function SignUpBody() {
   return (
