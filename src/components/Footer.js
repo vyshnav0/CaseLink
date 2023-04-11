8f8e402d990b4a27ef2c14ecefefa502a0349748
John Doe <johndoe@whoyou.com>
2023-04-11 10:51:01 +0530

    removed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import '../style/Footer.css'
-import {Link} from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div id = 'wholeOfFooter'>
-        <span id = "contactUs">Contact Us?</span>
-        <Link id='contactus' to='/contactinfo'>Click here</Link>
+    <div>
+        
     </div>
   )
 }
