847d459ee38a4b293ee51bacf3aa4cbf2ccbcac6
John Wick <wick@johnny.com>
2023-04-11 10:43:11 +0530

    final temp

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Body from '../components/Body'
+import Header from '../components/Header'
+
 
 export default function Home() {
   return (
     <div>
+        <div><Header></Header></div>
         <div><Body></Body></div>
         <div><Footer></Footer></div>
     </div>
