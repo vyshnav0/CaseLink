5c416526e0d2e401c0921eec8661b60647eb5d85
John Doe <johndoe@whoyou.com>
2023-04-11 09:39:16 +0530

    removed signup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
-import SignUp from './screens/SignUp';
 import SignOut from './screens/SignOut';
 import ContactUs from './screens/ContactUs'
 import {
