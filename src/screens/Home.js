4aaa278a8b41ed028c645bcc8ec463f35ac66cdd
John Doe <johndoe@whoyou.com>
2023-04-11 10:29:11 +0530

    Revert "added header"
    This reverts commit 6c182153137aac4dc5efc59818429badf43defff.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Body from '../components/Body'
-import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
-        <div><Header></Header></div>
         <div><Body></Body></div>
         <div><Footer></Footer></div>
     </div>
