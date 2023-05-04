8cdff2ca9959e70cb8a11a8d7f0de3fc424f6c94
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-04 16:14:50 +0530

    db reduced

--- backend/db.js
+++ backend/db.js
@@ -5,15 +5,7 @@ const mongoURI = "mongodb+srv://arjun:caselink@web4.twsr1jr.mongodb.net/caselink
 
 const mongoDB = () => {
   return mongoose.connect(mongoURI)
-    .then(async() => 
-        {
-            console.log('Connected to MongoDB Atlas')
-            const fetched_data = await mongoose.connection.db.collection("adminData")
-            fetched_data.find({}).toArray(function(err,data){
-                if(err) console.log(err);
-                else console.log(data);
-            })
-        })
+    .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.error(err));
 }
 
