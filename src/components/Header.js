e37169697968423447de16c8c4890fe5df32e1e3
Vyshnav <vyshnavk78@gmail.com>
2023-05-04 15:49:47 +0530

    changes HEader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import '../style/Header.css'
-import throttle from 'lodash/throttle';
+import React, { useEffect } from "react";
+import "../style/Header.css";
+import throttle from "lodash/throttle";
 
 export default function Header() {
   useEffect(() => {
@@ -14,40 +14,36 @@ export default function Header() {
       }
     }
     window.addEventListener("scroll", throttle(onScroll, 300));
-    return() => {
+    return () => {
       window.removeEventListener("scroll", throttle(onScroll, 300));
     };
-  },[]);
+  }, []);
+
   return (
     <div>
       <header className="header sticky sticky--top js-header">
-
-<div className="grid">
-
-  <nav className="navigation">
-    <ul className="navigation__list navigation__list--inline">
-      <li className="navigation__item"><a href="#" className="navigation__link navigation__link--is-active">Home</a></li>
-      <li className="navigation__item"><a href="#" className="navigation__link">About Us</a></li>
-      <li className="navigation__item"><a href="#" className="navigation__link">Work</a></li>
-      <li className="navigation__item"><a href="#" className="navigation__link">Clients</a></li>
-      <li className="navigation__item"><a href="#" className="navigation__link">Contact</a></li>
-    </ul>
-  </nav>
-
-</div>
-
-</header>
-
-<main className="main">
-<div className="grid">
-  <h1>Change this .</h1>
-  <p>Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! </p>
-  <p>Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! </p>
-  <p>Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! </p>
-  <p>Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! Death yeah, Murder yeah! </p>
-
-</div>
-</main>
+        <div className="grid">
+          <nav className="navigation">
+            <ul className="navigation__list navigation__list--inline">
+              <li className="navigation__item">
+                <a href="#" className="navigation__link navigation__link--is-active">Logo</a>
+              </li>
+              <li className="navigation__item">
+                <h1>CaseLink</h1>
+              </li>
+              <li className="navigation__item">
+                <a href="#" className="navigation__link">Work</a>
+              </li>
+              <li className="navigation__item">
+                <a href="#" className="navigation__link">Signup</a>
+              </li>
+              <li className="navigation__item">
+                <a href="#" className="navigation__link">Login</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
-        )
+  );
 }
