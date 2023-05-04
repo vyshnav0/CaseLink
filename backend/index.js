2d8d1ca5db24515fcc617cd71028259db06d1812
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-04 17:03:39 +0530

    added some backend code to SugnUpBody.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 5000
 const mongoDB = require("./db")
 
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 mongoDB()
 .then(() => {
   app.listen(port, () => {
--- src/components/SignUpBody.js
+++ src/components/SignUpBody.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../style/SignUpBody.css'
 
 export default function SignUpBody() {
+
+  const [credentials, setcredentials] = useState({username:"",fname:"",lname:"",age:"",sex:"",contactno:"",email:"",address:"",fathersName:"",mothersName:"",idNo:"",password:""})
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/createuser",{
+    method:'POST',
+    headers:{
+      'Content-Type' : 'application/json'
+    },
+    // possible error since age is not string yet initialised to string 
+    body:JSON.stringify({username:credentials.name,fname:credentials.fname,lname:credentials.lname,age:credentials.age,sex:credentials.sex,contactno:credentials.contactno,email:credentials.email,address:credentials.address,fathersName:credentials.fathersName,mothersName:credentials.mothersName,idNo:credentials.idNo,password:credentials.password})
+    })
+    const json = await response.json()
+    console.log(json)
+
+    if(!json.success){
+      alert("Enter valid credentials")
+    }
+  }
+
+  const onChange = (event)=>{
+    setcredentials({...credentials,[event.target.name] : event.target.value})
+  }
+
   return (
+    // points to note:
+    // outer <form onSubmit = {handleSubmit}> needed just after the outermost div & in each input element,
+    //  add attributes {name="(from line 6)" value={credentials.(respective name) onChange = {onChange}}}
     <div>Sign Up</div>
   )
 }
