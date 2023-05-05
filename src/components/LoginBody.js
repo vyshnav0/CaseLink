153c678e7217a5916a70b298c5e5b7760d2a7e2f
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-05 16:43:12 +0530

    biggest solution for the smallest problem

--- src/components/LoginBody.js
+++ src/components/LoginBody.js
@@ -6,7 +6,6 @@ export default function LoginBody() {
 
   const navigate = useNavigate()
   const [credentials, setcredentials] = useState({email:"",password:""})
-  const response = null
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
@@ -61,19 +60,19 @@ export default function LoginBody() {
     setcredentials({...credentials,[event.target.name] : event.target.value})
   }
   return (
-    <div> model of how to use form and input attributes
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email address</label>
-          <input name = "email" value = {credentials.email} onChange={onChange}></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input type = "password" name = "password" value = {credentials.password} onChange={onChange}></input>
-        </div>
-        <button>Login</button>
-      </form>
-    </div>
-    // <></>
+    // <div> model of how to use form and input attributes
+    //   <form onSubmit={handleSubmit}>
+    //     <div>
+    //       <label>Email address</label>
+    //       <input name = "email" value = {credentials.email} onChange={onChange}></input>
+    //     </div>
+    //     <div>
+    //       <label>Password</label>
+    //       <input type = "password" name = "password" value = {credentials.password} onChange={onChange}></input>
+    //     </div>
+    //     <button>Login</button>
+    //   </form>
+    // </div>
+    <></>
   )
 }
