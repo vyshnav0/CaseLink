0378b8d75edcfe3741713ae72a859639b27e6553
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-05 21:10:25 +0530

    File a complaint created

---
+++ src/screens/FileComplaint.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FileComplaintBody from '../components/FileComplaintBody'
+import Sidebar from '../components/Sidebar'
+
+export default function FileComplaint() {
+  return (
+    <div>
+        <Sidebar></Sidebar>
+        <FileComplaintBody></FileComplaintBody>
+    </div>
+  )
+}
