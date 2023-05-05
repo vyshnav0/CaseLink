10075f5ad0913a5a1f66c8553aa80302525c4d72
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-05 16:34:17 +0530

    biggest solution for the smallest problem

--- src/components/LoginBody.js
+++ src/components/LoginBody.js
@@ -5,28 +5,55 @@ import '../style/LoginBody.css'
 export default function LoginBody() {
 
   const navigate = useNavigate()
-  const [credentials, setcredentials] = useState({username:"",fname:"",lname:"",age:"",sex:"",contactno:"",email:"",address:"",fathersName:"",mothersName:"",idNo:"",password:""})
+  const [credentials, setcredentials] = useState({email:"",password:""})
+  const response = null
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/loginuser",{
-    method:'POST',
-    headers:{
-      'Content-Type' : 'application/json'
-    },
-    // possible error since age is not string yet initialised to string 
-    body:JSON.stringify({email:credentials.email,password:credentials.password})
-    })
-    const json = await response.json()
-    console.log(json)
+    const userOrOfficer = credentials.email.includes('@') ? 'user' : 'officer'
+    if(userOrOfficer=='user'){
+      const response = await fetch("http://localhost:5000/loginuser",{
+      method:'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body:JSON.stringify({email:credentials.email,password:credentials.password})
+      })
 
-    if(!json.success){
-      alert("Enter valid credentials")
+      const json = await response.json()
+      console.log(json)
+
+      if(!json.success){
+        alert("Enter valid credentials")
+      }
+      if(json.success){
+        localStorage.setItem("authToken",json.authToken);
+        console.log(localStorage.getItem("authToken"));
+        navigate("/");
+      }
+      console.log(`Welcome ${userOrOfficer} : ${credentials.email}`)
     }
-    if(json.success){
-      localStorage.setItem("authToken",json.authToken);
-      console.log(localStorage.getItem("authToken"));
-      navigate("/");
+    else{
+      const response = await fetch("http://localhost:5000/loginofficer",{
+      method:'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body:JSON.stringify({pen:credentials.email,password:credentials.password})
+      })
+
+      const json = await response.json()
+      console.log(json)
+
+      if(!json.success){
+        alert("Enter valid credentials")
+      }
+      if(json.success){
+        localStorage.setItem("authToken",json.authToken);
+        console.log(localStorage.getItem("authToken"));
+        navigate("/");
+      }
+      console.log(`Welcome ${userOrOfficer} : ${credentials.email}`)
     }
   }
 
@@ -34,19 +61,19 @@ export default function LoginBody() {
     setcredentials({...credentials,[event.target.name] : event.target.value})
   }
   return (
-    // <div> model of how to use form and input attributes
-    //   <form onSubmit={handleSubmit}>
-    //     <div>
-    //       <label>Email address</label>
-    //       <input name = "email" value = {credentials.email} onChange={onChange}></input>
-    //     </div>
-    //     <div>
-    //       <label>Password</label>
-    //       <input type = "password" name = "password" value = {credentials.password} onChange={onChange}></input>
-    //     </div>
-    //     <button>Login</button>
-    //   </form>
-    // </div>
-    <></>
+    <div> model of how to use form and input attributes
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email address</label>
+          <input name = "email" value = {credentials.email} onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type = "password" name = "password" value = {credentials.password} onChange={onChange}></input>
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+    // <></>
   )
 }
