8091f87c1b36c9222ca6fcb6b05c4c4a2aea9cba
Arjun Vinod <arjunvinod09@gmail.com>
2023-05-08 15:20:35 +0530

    minor header change

--- src/components/FileComplaintBody.js
+++ src/components/FileComplaintBody.js
@@ -9,7 +9,7 @@ export default function FileComplaint() {
             const response = await fetch("http://localhost:5000/complaintauth" , {
                 method:"GET",
                 headers:{
-                    Accept : "*/*",
+                    Accept : "application/json",
                     "Content-Type" : "application/json"
                 },
                 credentials : "include"
