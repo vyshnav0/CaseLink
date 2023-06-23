40a6ff85bef1f748de11d8b6b60b8aa15e1abf91
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-23 17:17:24 +0530

    rm sidebar

--- src/screens/FileComplaint.js
+++ src/screens/FileComplaint.js
@@ -4,7 +4,6 @@ import FileComplaintBody from '../components/FileComplaintBody'
 export default function FileComplaint() {
   return (
     <div>
-        <Sidebar></Sidebar>
         <FileComplaintBody></FileComplaintBody>
     </div>
   )
