9a9bde310d71b38bef52ab3adbb0cd7b60be1fc7
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-24 11:40:29 +0530

    signup done

--- src/components/SignUpOfficer.js
+++ src/components/SignUpOfficer.js
@@ -16,22 +16,22 @@ import {Link,useNavigate} from 'react-router-dom'
 export default function SignUpOfficer() {
 
   const navigate = useNavigate()
-  const [credentials, setcredentials] = useState({username:"",fname:"",lname:"",age:"",sex:"",contactno:"",pen:"",station:"",password:""})
+  const [credentials, setcredentials] = useState({fname:"",lname:"",age:"",sex:"",contactno:"",pen:"",station:"",rank:"",password:""})
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/createuser",{
+    const response = await fetch("http://localhost:5000/createofficer",{
     method:'POST',
     headers:{
       'Content-Type' : 'application/json'
     },
-    body:JSON.stringify({username:credentials.username,fname:credentials.fname,lname:credentials.lname,age:credentials.age,sex:credentials.sex,contactno:credentials.contactno,pen:credentials.pen,station:credentials.station,password:credentials.password})
+    body:JSON.stringify({fname:credentials.fname,lname:credentials.lname,age:credentials.age,sex:credentials.sex,contactno:credentials.contactno,pen:credentials.pen,station:credentials.station,rank:credentials.rank,password:credentials.password})
     })
     const json = await response.json()
     console.log(json)
 
     if(!json.success){
-      alert("Enter valid credentials")
+      alert("There was an error in creating Officer profile")
     }
     if(json.success){
       navigate("/login");
@@ -58,12 +58,8 @@ export default function SignUpOfficer() {
   };
 
   return (
-    // points to note:
-    // outer <form onSubmit = {handleSubmit}> needed just after the outermost div & in each input element,
-    //  add attributes {name="(from line 6)" value={credentials.(respective name) onChange = {onChange}}}
     <div>
       <form onSubmit={handleSubmit}>
-       
        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
 <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
@@ -85,7 +81,7 @@ export default function SignUpOfficer() {
 
   <MDBInput wrapperClass='mb-4' label='First Name' name='fname' type='text' value = {credentials.fname} onChange={onChange} required />
       <MDBInput wrapperClass='mb-4' label='Last Name' name='lname' type='text' value = {credentials.lname} onChange={onChange} required />
-  <MDBInput wrapperClass='mb-4' label='PEN Number' name='pen' type='text' value = {credentials.fname} onChange={onChange}/>
+  <MDBInput wrapperClass='mb-4' label='PEN Number' name='pen' type='text' value = {credentials.pen} onChange={onChange}/>
 <MDBInput wrapperClass='mb-4' label='Password' name='password' type='password' value = {credentials.password} onChange={onChange} required />
 <MDBInput wrapperClass='mb-4' label='Age' name='age' type='text' value = {credentials.age} onChange={onChange} required />
 <MDBInput wrapperClass='mb-4' label='Sex' name='sex' type='text' value = {credentials.sex} onChange={onChange} required />
