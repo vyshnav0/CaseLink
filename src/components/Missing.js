639250796919de5e6931ce8a08905182ee6984e8
Vyshnav <vyshnavk78@gmail.com>
2023-06-25 14:48:34 +0530

    0

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {Autoplay, Pagination, Navigation, FreeMode } from "swiper";
+import {Pagination, Navigation, FreeMode } from "swiper";
 import 'swiper/css';
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -11,6 +11,17 @@ import PersonCard from './PersonCard'
 
 
 export default function Missing(){
+    const missingPersons = [
+        // Replace this array with actual data from your "missing" database
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 1', age: '25' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 2', age: '30' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 3', age: '35' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 4', age: '40' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 5', age: '45' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 6', age: '50' },
+        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 7', age: '55' },
+        // Add more entries as needed
+      ];
     return (
         <>
             <div className= "container py-4 px-4 justify-content-center bg-secondary" style={{ maxWidth: '70%' }}>
@@ -39,26 +50,28 @@ export default function Missing(){
                 }}
                 freeMode = {true}
                 grabCursor = {true}
-                autoplay={{
-                    delay: 900,
-                    disableOnInteraction: false,
-                }}
+            
                 loop={true}
                 pagination={{
                     clickable: true,
                 }}
                 navigation={true}
-                modules={[Autoplay, Pagination, FreeMode, Navigation]}
+                modules={[ Pagination, FreeMode, Navigation]}
                 className = "mySwiper"
                 >
                     {/* CHANGE THE BELOW TO TAKE THE RECENT 7 INPUTS FROM THE MISSING DB.*/} 
                     
-                    {[...Array(7)].map((_, index) => (
+                    {/* {[...Array(7)].map((_, index) => (
                         <SwiperSlide key={index}>
                             <PersonCard data={{ imgSrc: 'https://via.placeholder.com/150', title: `Missing person${index + 1}`, age: '25' }} />
                         </SwiperSlide>
-                    ))}
-                    
+                    ))} */}
+                {missingPersons.map((person, index) => (
+            <SwiperSlide key={index}>
+              <PersonCard data={person} />
+            </SwiperSlide>
+          ))}
+
                 </Swiper>
             </div>
         </>
