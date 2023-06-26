0113f4e335b7094c82797a2f2b7d37e0dbde5d83
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-26 20:14:37 +0530

    frontend missing get

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,80 +1,97 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Pagination, Navigation, FreeMode } from "swiper";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, FreeMode } from 'swiper';
 import 'swiper/css';
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import 'swiper/css/free-mode';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import PersonCard from './PersonCard'
+import PersonCard from './PersonCard';
 
+export default function Missing() {
+  const [fullname, setFullname] = useState([]);
+  
+  const callMissingPage = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/obtainmissing', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      const data = await response.json();
+      const updatedFullname = data.fname.map((fname, index) => {
+        const lname = data.lname[index].lname;
+        return `${fname.fname} ${lname}`;
+      });
+      setFullname(updatedFullname);
+    } catch (err) {
+      console.log(err);
+      console.log('There was an error during fetching missing persons details');
+    }
+  };
 
-export default function Missing(){
-    const missingPersons = [
-        // Replace this array with actual data from your "missing" database
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 1', age: '25' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 2', age: '30' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 3', age: '35' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 4', age: '40' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 5', age: '45' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 6', age: '50' },
-        { imgSrc: 'https://via.placeholder.com/150', title: 'Missing person 7', age: '55' },
-        // Add more entries as needed
-      ];
-    return (
-        <>
-            <div className= "container py-4 px-4 justify-content-center bg-secondary" style={{ maxWidth: '70%' }}>
-                <Swiper
-                breakpoints = {{
-                    0:{
-                        slidesPerView: 1,
-                        spaceBetween: 10,
-                    },
-                    480:{
-                        slidesPerView: 2,
-                        spaceBetween: 10,
-                    },
-                    768:{
-                        slidesPerView: 3,
-                        spaceBetween: 15,
-                    },
-                    1024:{
-                        slidesPerView: 4,
-                        spaceBetween: 15,
-                    },
-                    1280:{
-                        slidesPerView: 4,
-                        spaceBetween: 30,
-                    },
-                }}
-                freeMode = {true}
-                grabCursor = {true}
-            
-                loop={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[ Pagination, FreeMode, Navigation]}
-                className = "mySwiper"
-                >
-                    {/* CHANGE THE BELOW TO TAKE THE RECENT 7 INPUTS FROM THE MISSING DB.*/} 
-                    
-                    {/* {[...Array(7)].map((_, index) => (
-                        <SwiperSlide key={index}>
-                            <PersonCard data={{ imgSrc: 'https://via.placeholder.com/150', title: `Missing person${index + 1}`, age: '25' }} />
-                        </SwiperSlide>
-                    ))} */}
-                {missingPersons.map((person, index) => (
+  useEffect(() => {
+    callMissingPage();
+  }, []);
+
+  useEffect(() => {
+    const missingPersons = fullname.map((name, index) => ({
+      imgSrc: 'https://via.placeholder.com/150',
+      title: name,
+      age: '25', // Update the age as needed
+    }));
+    setMissingPersons(missingPersons);
+  }, [fullname]);
+
+  const [missingPersons, setMissingPersons] = useState([]);
+
+  return (
+    <>
+      <div className="container py-4 px-4 justify-content-center bg-secondary" style={{ maxWidth: '70%' }}>
+        <Swiper
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 15,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 15,
+            },
+            1280: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          freeMode={true}
+          grabCursor={true}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, FreeMode, Navigation]}
+          className="mySwiper"
+        >
+          {missingPersons.map((person, index) => (
             <SwiperSlide key={index}>
               <PersonCard data={person} />
             </SwiperSlide>
           ))}
-
-                </Swiper>
-            </div>
-        </>
-    )
-
+        </Swiper>
+      </div>
+    </>
+  );
 }
