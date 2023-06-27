209a0e9399c4bf8726debb76ed13def2b48b5f90
Yadukrishna T K <yadutk80@gmail.com>
2023-06-27 11:20:49 +0530

    modified_swiper

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from 'react-bootstrap'
 import '../style/PersonCard.css'
 
 const PersonCard = props => {
     let {imgSrc, age, title} = props.data;
     return(
-        <>
-          <Card className ="p-0 overflow-hidden h-100 shadow">
-            <div className="overflow-hidden rounded p-0 bg-light">
-                <Card.Img variant="top" src = {imgSrc}/>
+         <div className="slide-container swiper">
+            <div className="slide-content">
+                <div className="card-wrapper swiper-wrapper">
+                    
+                    <div className="card swiper-slide">
+                        <div className="image-content">
+                            <span className="overlay"></span>
+
+                            <div className="card-image">
+                                <img src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1687841652~exp=1687842252~hmac=47785435afa58b0ed6d4b9e1a31e56e89cad1cbf34c97af34a9885ebb299c3fd" alt="" class="card-img"/>
+                            </div>
+                        </div>
+
+                        <div className="card-content">
+                            <h2 className="name">{title}</h2>
+                            <p className="age">{age}</p>
+                        </div>
+                    </div>
+                   
+                </div>
             </div>
-            <Card.Body className="text-center">
-                 <Card.Title>{title}</Card.Title>
-                 <Card.Title className = "display-9">{age}</Card.Title>
-            </Card.Body>
-          </Card>
-        </>
+
+        </div>
     ) 
 
 }
