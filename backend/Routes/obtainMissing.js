0ff344d23c3507da49f8b01411d6d1da0fdce44e
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-27 10:48:23 +0530

    wanted schema

--- backend/Routes/obtainMissing.js
+++ backend/Routes/obtainMissing.js
@@ -2,29 +2,31 @@ const express = require('express')
 const router = express.Router()
 const Missing = require('../models/Missing')
 
-router.get("/obtainmissing", async (req, res) => {
-    try{
-        const fname = await Missing.find({},{fname:1,_id:0}).sort({_id: -1}).limit(8)
-        const lname = await Missing.find({},{lname:1,_id:0}).sort({_id: -1}).limit(8)
-        const age = await Missing.find({},{age:1,_id:0}).sort({_id: -1}).limit(8)
-        res.json({success:true,fname:fname,lname:lname,age:age})
-    }
-    catch(err){
-        res.json({success:false})
-        console.error(err);
-    }
-})
-
 router.post("/addmissing", async (req,res) => {
     try{
         await Missing.create({
             fname : req.body.fname,
             lname : req.body.lname,
-            age : req.body.age
+            age : req.body.age,
+            img : req.body.img
         })
         res.json({success:true})
     }
     catch(err){
+        console.error(err);
+    }
+})
+
+router.get("/obtainmissing", async (req, res) => {
+    try{
+        const fname = await Missing.find({},{fname:1,_id:0}).sort({_id: -1}).limit(8)
+        const lname = await Missing.find({},{lname:1,_id:0}).sort({_id: -1}).limit(8)
+        const age = await Missing.find({},{age:1,_id:0}).sort({_id: -1}).limit(8)
+        const img = await Missing.find({},{img:1,_id:0}).sort({_id: -1}).limit(8)
+        res.json({success:true,fname:fname,lname:lname,age:age,img:img})
+    }
+    catch(err){
+        res.json({success:false})
         console.error(err);
     }
 })
---
+++ backend/Routes/obtainWanted.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+const Wanted = require('../models/Wanted')
+
+router.get("/obtainwanted", async (req,res) => {
+    try{
+        const fname = await Wanted.find({},{fname:1,_id:0}).sort({_id:-1}).limit(8)
+        const lname = await Wanted.find({},{lname:1,_id:0}).sort({_id:-1}).limit(8)
+        const age = await Wanted.find({},{age:1,_id:0}).sort({_id:-1}).limit(8)
+        const img = await Wanted.find({},{image:1,_id:0}).sort({_id:-1}).limit(8)
+        res.json({success:true,fname:fname,lname:lname,age:age,img:img})
+    }
+    catch(err){
+        console.error(err);
+        res.json({success:false})
+    }
+})
+
+router.post('/addwanted' , async (req,res) => {
+    try{
+        await Wanted.create({
+            fname : req.body.fname,
+            lname : req.body.lname,
+            age : req.body.age,
+            img : req.body.img
+        })
+        res.json({success:true})
+    }
+    catch(err){
+        console.error(err)
+    }
+})
+
+module.exports = router
