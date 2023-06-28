a004cd4d97eff7a33f1618ccb274a5b1fd86c423
Yadukrishna T K <yadutk80@gmail.com>
2023-06-28 11:53:03 +0530

    bodycreated

--- src/screens/OfficerWork.js
+++ src/screens/OfficerWork.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import {Link,useNavigate} from 'react-router-dom'
+import React from 'react'
+import Header from '../components/Header'
+import OfficerWorkBody from '../components/OfficerWorkbody'
 
+export default function Profile() {
+  return (
+    <div>
+      <Header></Header>
+      <OfficerWorkBody></OfficerWorkBody>
 
-export default function OfficerWork(){
-    return(
-        <>
-        <h1>officer duties</h1>
-        <h2>managing databases</h2>
-        <h2>complaints to FIR</h2>
-        <h3>baki nala noka</h3>
-        </>
-    )
+    </div>
+  )
 }
