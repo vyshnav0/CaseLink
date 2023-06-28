cd1ed834d418f70d2140b269ef54c4ea9a486086
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-28 20:30:23 +0530

    mis and want limit 8 removed

--- backend/Routes/obtainWanted.js
+++ backend/Routes/obtainWanted.js
@@ -4,16 +4,16 @@ const Wanted = require('../models/Wanted')
 
 router.get("/obtainwanted", async (req,res) => {
     try{
-        const fname = await Wanted.find({},{fname:1,_id:0}).sort({_id:-1}).limit(8)
-        const lname = await Wanted.find({},{lname:1,_id:0}).sort({_id:-1}).limit(8)
-        const age = await Wanted.find({},{age:1,_id:0}).sort({_id:-1}).limit(8)
-        const img = await Wanted.find({},{image:1,_id:0}).sort({_id:-1}).limit(8)
-        const gender = await Wanted.find({},{gender:1,_id:0}).sort({_id:-1}).limit(8);
-        const height = await Wanted.find({},{height:1,_id:0}).sort({_id:-1}).limit(8);
-        const weight = await Wanted.find({},{weight:1,_id:0}).sort({_id:-1}).limit(8);
-        const location = await Wanted.find({},{location:1,_id:0}).sort({_id:-1}).limit(8);
-        const date = await Wanted.find({},{date:1,_id:0}).sort({_id:-1}).limit(8);
-        const contactno = await Wanted.find({},{contactno:1,_id:0}).sort({_id:-1}).limit(8);
+        const fname = await Wanted.find({},{fname:1,_id:0}).sort({_id:-1});
+        const lname = await Wanted.find({},{lname:1,_id:0}).sort({_id:-1});
+        const age = await Wanted.find({},{age:1,_id:0}).sort({_id:-1});
+        const img = await Wanted.find({},{image:1,_id:0}).sort({_id:-1});
+        const gender = await Wanted.find({},{gender:1,_id:0}).sort({_id:-1});
+        const height = await Wanted.find({},{height:1,_id:0}).sort({_id:-1});
+        const weight = await Wanted.find({},{weight:1,_id:0}).sort({_id:-1});
+        const location = await Wanted.find({},{location:1,_id:0}).sort({_id:-1});
+        const date = await Wanted.find({},{date:1,_id:0}).sort({_id:-1});
+        const contactno = await Wanted.find({},{contactno:1,_id:0}).sort({_id:-1});
 
         res.json({success:true,fname:fname,lname:lname,age:age,gender:gender,img:img,height:height,weight:weight,location:location,date:date,contactno:contactno})
     }
