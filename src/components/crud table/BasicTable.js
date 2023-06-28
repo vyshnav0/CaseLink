1b78e3d924b6c6791cd6c0f88e48721bf03b6be1
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-28 21:15:47 +0530

    table takes db data

--- src/components/crud table/BasicTable.js
+++ src/components/crud table/BasicTable.js
@@ -1,127 +1,211 @@
-import React, { useState, useEffect } from 'react';
-import MOCK_DATA from './MOCK_DATA.json'
+import React, { useState, useEffect, useRef } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function TableRows({ rows, tableRowRemove, onValUpdate }) {
-    return rows.map((rowsData, index) => {
-      const { id, full_name, age, gender, height, weight, last_seen_location, last_seen_date, contact_number } = rowsData;
-      return (
-        <tr key={index}>
-          <td>
-            <input
-              type="text"
-              value={id}
-              onChange={(event) => onValUpdate(index, event)}
-              name="id"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={full_name}
-              onChange={(event) => onValUpdate(index, event)}
-              name="full_name"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={age}
-              onChange={(event) => onValUpdate(index, event)}
-              name="age"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={gender}
-              onChange={(event) => onValUpdate(index, event)}
-              name="gender"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={height}
-              onChange={(event) => onValUpdate(index, event)}
-              name="height"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={weight}
-              onChange={(event) => onValUpdate(index, event)}
-              name="weight"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={last_seen_location}
-              onChange={(event) => onValUpdate(index, event)}
-              name="last_seen_location"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={last_seen_date}
-              onChange={(event) => onValUpdate(index, event)}
-              name="last_seen_date"
-              className="form-control"
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              value={contact_number}
-              onChange={(event) => onValUpdate(index, event)}
-              name="contact_number"
-              className="form-control"
-            />
-          </td>
+  return rows.map((rowData, index) => {
+    const { id, full_name, age, gender, height, weight, last_seen_location, last_seen_date, contact_number } = rowData;
+    return (
+      <tr key={index}>
+        <td>
+          <input
+            type="text"
+            value={id}
+            onChange={(event) => onValUpdate(index, event)}
+            name="id"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={full_name}
+            onChange={(event) => onValUpdate(index, event)}
+            name="full_name"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={age}
+            onChange={(event) => onValUpdate(index, event)}
+            name="age"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={gender}
+            onChange={(event) => onValUpdate(index, event)}
+            name="gender"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={height}
+            onChange={(event) => onValUpdate(index, event)}
+            name="height"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={weight}
+            onChange={(event) => onValUpdate(index, event)}
+            name="weight"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={last_seen_location}
+            onChange={(event) => onValUpdate(index, event)}
+            name="last_seen_location"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={last_seen_date}
+            onChange={(event) => onValUpdate(index, event)}
+            name="last_seen_date"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={contact_number}
+            onChange={(event) => onValUpdate(index, event)}
+            name="contact_number"
+            className="form-control"
+          />
+        </td>
+        <td>
+          <button
+            className="btn btn-dark"
+            onClick={() => tableRowRemove(index)}
+          >
+            Remove
+          </button>
+        </td>
+      </tr>
+    );
+  });
+}
 
+export const BasicTable = () => {
+  let data = [{}];
+  const effectRan = useRef(false);
+  const [fullname, setFullname] = useState([]);
+  const [missingPersons, setMissingPersons] = useState([]);
+  const [rows, setRows] = useState([]);
 
-          <td>
-            <button
-              className="btn btn-dark"
-              onClick={() => tableRowRemove(index)}
-            >
-              Remove
-            </button>
-          </td>
-        </tr>
-      );
-    });
+  const callMissingPage = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/obtainmissing', {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+      data = await response.json();
+      console.log("This is the data obtained");
+      console.log(data);
+      const updatedFullname = data.fname.map((fname, index) => {
+        const lname = data.lname[index].lname;
+        const fullname = data.fname[index] + " " + lname
+        const age = data.age[index].age;
+        const gender = data.gender[index].gender;
+        const height = data.height[index].height;
+        const weight = data.weight[index].weight;
+        const location = data.location[index].location;
+        const date = data.date[index].date;
+        const contactno = data.contactno[index].contactno;
+        return {
+          fullName: fullname,
+          age: age,
+          gender: gender,
+          height: height,
+          weight: weight,
+          last_seen_location: location,
+          last_seen_date: date,
+          contact_number: contactno
+        };
+      });
+      setFullname(updatedFullname);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-export const BasicTable = () =>{
-    
-    const [rows, setRows] = useState([]);
-    useEffect(() => {
-        setRows(MOCK_DATA)
-      }, []
-      );
+  useEffect(() => {
+    const missingPersons = fullname.map((name) => ({
+      fullName: name.fullName,
+      age: name.age,
+      gender: name.gender,
+      height: name.height,
+      weight: name.weight,
+      last_seen_location: name.last_seen_location,
+      last_seen_date: name.last_seen_date,
+      contact_number: name.contact_number,
+    }));
+    setMissingPersons(missingPersons);
+  }, [fullname]);
+
+  missingPersons.map((person,index) => {
+    const full_name = person.fullName;
+    const age = person.age;
+    const gender = person.gender;
+    const height = person.height;
+    const weight = person.weight;
+    const lastSeenLocation = person.last_seen_location;
+    const lastSeenDate = person.last_seen_date;
+    const contactNumber = person.contact_number;
+
+    let personData = {
+      full_name: full_name,
+      age: age,
+      gender: gender,
+      height: height,
+      weight: weight,
+      last_seen_location: lastSeenLocation,
+      last_seen_date: lastSeenDate,
+      contact_number: contactNumber,
+    }
+    return personData
+  })
+
+  useEffect(() => {
+    if (effectRan.current === false) {
+      callMissingPage();
+      effectRan.current = true;
+    }
+    console.log("And this is the data being set");
+    console.log(missingPersons);
+    setRows(missingPersons)
+  }, [missingPersons]);
+
   const addRowTable = () => {
     const newRow = {
-        id: "",
-        full_name: "",
-        age: "",
-        gender: "",
-        height: "",
-        weight: "",
-        last_seen_location: "",
-        last_seen_date: "",
-        contact_number: "",
-      };
+      id: "",
+      full_name: "",
+      age: "",
+      gender: "",
+      height: "",
+      weight: "",
+      last_seen_location: "",
+      last_seen_date: "",
+      contact_number: "",
+    };
     setRows([...rows, newRow]);
   };
 
@@ -137,6 +221,7 @@ export const BasicTable = () =>{
     updatedRows[index][name] = value;
     setRows(updatedRows);
   };
+
   return (
     <>
       <table className="table table-striped">
@@ -151,7 +236,6 @@ export const BasicTable = () =>{
             <th>Location</th>
             <th>Date</th>
             <th>Contact Info</th>
-
             <th>
               <button className="btn btn-danger" onClick={addRowTable}>
                 Add Entry
