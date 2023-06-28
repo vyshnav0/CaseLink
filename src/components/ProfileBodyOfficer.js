248fcde9cc02b97d0396fad37293a7ff24442bb7
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-28 16:19:07 +0530

    profile data extracted

--- src/components/ProfileBodyOfficer.js
+++ src/components/ProfileBodyOfficer.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 export default function ProfileBodyOfficer() {
+
+  const data = JSON.parse(localStorage.getItem(data))
+  const pen = data.pen
+  const name = data.fname + " " + data.lname
+  const age = data.age;
+  const sex = data.sex;
+  const rank = data.rank;
+  const contactno = data.contactno;
+  const station = data.station;
+
   return (
     <div>
       ProfileBodyOfficer
--- src/components/ProfileBodyUser.js
+++ src/components/ProfileBodyUser.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export default function ProfileBody() {
+
+  const data = JSON.parse(localStorage.getItem('data'));
+  const username = data.username;
+  const name = data.fname + " " + data.lname
+  const age = data.age;
+  const sex = data.sex;
+  const contactno = data.contactno;
+  const email = data.email;
+  const address = data.address;
+  const fathersName = data.fathersName;
+  const mothersName = data.mothersName;
+
   return (
     <div>
       ProfileBodyUser
