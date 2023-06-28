e3bbdd8e1859181d29acb500f154b19c36551d7d
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-28 15:50:45 +0530

    Profile auth diff pages for user and officer body

---
+++ src/components/ProfileBodyOfficer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProfileBodyOfficer() {
+  return (
+    <div>
+      ProfileBodyOfficer
+    </div>
+  )
+}
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState , useEffect , useRef} from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
-import ProfileBody from '../components/ProfileBody'
+import ProfileBodyUser from '../components/ProfileBodyUser'
+import ProfileBodyOfficer from '../components/ProfileBodyOfficer'
 
 export default function Profile() {
+
+  const navigate = useNavigate()
+  const userOrOfficer = localStorage.getItem("usertype");
+  const effectRan = useRef(false)
+  const callComplaintPage = async() => {
+    try {
+        console.log("Going to fetch from complaintauth");
+        console.log(`User logged in is: ${userOrOfficer}`);
+        const response = await fetch(`http://localhost:5000/complaintauth?authToken=${localStorage.getItem("authToken")}` , 
+        {
+            method:"GET",
+            headers:{
+                Accept : "application/json",
+                "Content-Type" : "application/json"
+            },
+            credentials : "include"
+        });
+        const data = await response.json();
+        console.log(data);
+        console.log("Returned from fetching comlpaintauth");
+
+
+        if(!response.status(200)){
+            console.log("There was an error in authentication")
+        }
+    } catch (error) {
+        console.log("There was an error in authenticating user");
+        navigate("/login");
+    }
+  }
+
+  useEffect(() => {
+    if(effectRan.current === false){
+        callComplaintPage()
+        effectRan.current = true
+    }
+}, [])
+
   return (
     <div>
       <Header></Header>
-      <ProfileBody></ProfileBody>
-
+      {userOrOfficer === "user"? <ProfileBodyUser></ProfileBodyUser> : <ProfileBodyOfficer></ProfileBodyOfficer>}
     </div>
   )
 }
