c855319969e0a9ee819709471e75017db8b7759d
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-29 23:29:42 +0530

    DB SWITCHED TO PRESENT

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://arjun:caselink@web4.twsr1jr.mongodb.net/caselink?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://arjunvinod09:calender5%40T@clusterdeploy.xtrgodc.mongodb.net/"
 // const mongoURI = "mongodb://arjun:<password>@ac-vv6p1zl-shard-00-00.twsr1jr.mongodb.net:27017,ac-vv6p1zl-shard-00-01.twsr1jr.mongodb.net:27017,ac-vv6p1zl-shard-00-02.twsr1jr.mongodb.net:27017/?ssl=true&replicaSet=atlas-xsnwl4-shard-0&authSource=admin&retryWrites=true&w=majority"
 // if the database faces connection issues swap out the mongoURI
 
