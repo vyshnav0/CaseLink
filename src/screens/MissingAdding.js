be4531c82ab457d90c963b0e54a81a2f1e9bb45a
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-29 12:36:51 +0530

    added new screens

---
+++ src/screens/MissingAdding.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+import MissingAddingBody from '../components/MissingAddingBody'
+
+export default function MissingAdding() {
+  return (
+    <div>
+        <Header></Header>
+        <MissingAddingBody></MissingAddingBody>
+    </div>
+  )
+}
