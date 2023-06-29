1537f10b3953fabdb88bc4c2739cea0662ab5675
Arjun Vinod <arjunvinod09@gmail.com>
2023-06-29 18:10:36 +0530

    graph style

--- src/screens/Analysis.js
+++ src/screens/Analysis.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Chart from 'react-apexcharts';
-
+import Header from '../components/Header'
 
 export default function Analysis() {
     const series = [
@@ -24,8 +24,41 @@ export default function Analysis() {
       };
   return (
     <div>
+      <Header></Header>
         {/* <p>hi</p> */}
-        <Chart options={options} type="bar" series={series} width="70%" />
+        <Chart  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} options={options} type="bar" series={series} width="60%" />
     </div>
   )
 }
+
+
+// import React from 'react';
+// import Chart from 'react-apexcharts';
+// import Header from '../components/Header';
+
+// export default function Analysis() {
+//   const series = [
+//     {
+//       name: 'No. of reported cases', // will be displayed on the y-axis
+//       data: [43, 53, 50, 57],
+//     },
+//   ];
+//   const options = {
+//     chart: {
+//       id: 'caselink_report',
+//     },
+//     xaxis: {
+//       categories: ['Vandalsim', 'Robbery', 'Murder', 'Attempt to Murder'], // will be displayed on the x-axis
+//     },
+//   };
+
+//   return (
+//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+//       <div>
+//         <Header />
+//         {/* <p>hi</p> */}
+//         <Chart options={options} type="bar" series={series} width="40%" />
+//       </div>
+//     </div>
+//   );
+// }
