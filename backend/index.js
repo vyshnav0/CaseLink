ab8a10a20b0bd8e3a8ca6e74dc59bf38d8ed8537
Arjun Vinod <arjunvinod09@gmail.com>
2023-08-01 11:31:03 +0530

    setheaders to allow delete method

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ app.use(bodyparser.urlencoded({extended:true,parameterLimit:100000,limit:"500mb"
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,DELETE")
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
