984e7a2b6ac5cac9be83db8c11f221ca3787cc75
Arjun Vinod <arjunvinod09@gmail.com>
2023-08-03 18:05:24 +0530

    refresh button added to the header for officers

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,17 @@ import '../style/Header.css'
 import {NavLink} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-import {Link} from 'react-router-dom'
+import {Link , useNavigate} from 'react-router-dom'
 
 export default function Header() {
+  const navigate = useNavigate()
   const logged = localStorage.getItem("usertype");
   const status = logged === "user" || logged === "officer";
-  let officer = false
+  let [officer,pen] = [false,null]
   officer = logged === "officer"
+  if(officer){
+    pen = JSON.parse(localStorage.getItem("data")).pen
+  }
 
   function logout(){
     localStorage.removeItem("authToken")
@@ -19,8 +23,28 @@ export default function Header() {
     localStorage.removeItem("complaintstatus")
   }
 
+  const refreshofficer = async()=> {
+    try {
+      const ref = await fetch('http://localhost:5000/refreshofficer',{
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json",
+          accept : "application/json"
+        },
+        body:JSON.stringify({pen:pen})
+      })
 
-  
+      const res = await ref.json()
+      const data = res.data
+      localStorage.setItem("data",data)
+      console.log("Data refreshed succesfully");
+      navigate("/profile")
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <div className='header'>
@@ -37,6 +61,7 @@ export default function Header() {
           {/* <li><Link to='/contactinfo'>Contact Us</Link></li> */}
           {/* <li><Link to='/login' onClick={logout}>{status ? 'Sign out' : 'Login'}</Link></li> */}
         </div>
+        {officer && <li className='header_options listitems'><Button onClick={refreshofficer}>Refresh</Button></li>}
         <Link to='/login' onClick={logout}><Button className='log' variant='dark'>{status ?'Log out' : 'Log in'}</Button></Link>
       </div>
     </>
