150f75d8c48e339bd0f55feaa27653a44bff02be
Yadukrishna T K <yadutk80@gmail.com>
2023-08-03 08:06:54 +0530

    modified profile

--- src/components/ProfileBodyOfficer.js
+++ src/components/ProfileBodyOfficer.js
@@ -15,94 +15,52 @@ export default function ProfileBodyOfficer() {
   const cases = data.opencases;
 
   return (
-  <div>
-  <div className='cont'>
-    <div className='al'>
-    <div className='car'>
-      <div class="col-lg-2 custom-height ">
-        <div class="card mb-4">
-          <div class="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-              class="rounded-circle img-fluid" width='100px'/>
-            <h3 class="my-3">{name}</h3>
-            <p class="text-muted mb-1">{pen}</p>
-            {/* <div class="d-flex justify-content-center mb-2">
-              <button type="button" class="btn btn-outline-primary ">Edit Profile</button>
-            </div> */}
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='pro'>
-    <div class="col-lg-10">
-        <div class="card mb-4">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Pen Number</p>
+  <div className='pro'>
+  <section class="vh-100" >
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-top h-100">
+      <div class="col col-md-9 col-lg-7 col-xl-5">
+        <div class="card">
+          <div class="card-body p-4">
+            <div class="d-flex text-black">
+              <div class="flex-shrink-0">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  alt="Generic placeholder image" class="img-fluid"/>
               </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{pen}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">age</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{age}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Sex</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{sex}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Mobile no</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{contactno}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Designation</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{rank}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Station</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{station}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Ongoing Investigations</p>
-              </div>
-              <div class="col-sm-9">
-                {cases.map((ongoing,index) => (
-                  <div class="row">
-                    <div class="col-sm-9">
-                      <p class="text-muted mb-0">{ongoing}</p>
-                    </div>
+              <div class="flex-grow-1 ms-3">
+                <h2 class="mb-1">{name}</h2>
+                <p class="mb-2 pb-1" >Pen : {pen}</p>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Age</p>
+                    <p class="mb-0">{age}</p>
                   </div>
-                ))}
+                  <div class="px-3">
+                    <p class="small text-muted mb-1">Gender</p>
+                    <p class="mb-0">{sex}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Contact Number</p>
+                    <p class="mb-0">{contactno}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Designation</p>
+                    <p class="mb-0">{rank}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Station</p>
+                    <p class="mb-0">{station}</p>
+                  </div>
+                </div>
+                <div class="d-flex pt-1">
+                  <button type="button" class="btn btn-outline-primary me-1 flex-grow-1">Edit Profile</button>
+                </div>
               </div>
             </div>
           </div>
@@ -110,7 +68,7 @@ export default function ProfileBodyOfficer() {
       </div>
     </div>
   </div>
-  </div>
-  </div>
+</section>
+</div>
   )
 }
--- src/components/ProfileBodyUser.js
+++ src/components/ProfileBodyUser.js
@@ -16,88 +16,62 @@ export default function ProfileBody() {
   const mothersName = data.mothersName;
 
   return (
-    <div>
-  <div className='cont'>
-    <div className='al'>
-    <div className='car'>
-      <div class="col-lg-2 custom-height ">
-        <div class="card mb-4">
-          <div class="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-              class="rounded-circle img-fluid" width='100px'/>
-            <h3 class="my-3">{name}</h3>
-            <p class="text-muted mb-1">{username}</p>
-            <div class="d-flex justify-content-center mb-2">
-              <button type="button" class="btn btn-outline-primary ">Edit Profile</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     <div className='pro'>
-    <div class="col-lg-10">
-        <div class="card mb-4">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Email</p>
+  <section class="vh-100" >
+  <div class="container py-5">
+    <div class="row d-flex justify-content-center align-items-top ">
+      <div class="col col-md-9 col-lg-7 col-xl-5">
+        <div class="card">
+          <div class="card-body p-4">
+            <div class="d-flex text-black">
+              <div class="flex-shrink-0">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  alt="Generic placeholder image" class="img-fluid"/>
               </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{email}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">age</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{age}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Sex</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{sex}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Mobile no</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{contactno}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Address</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{address}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Father's Name</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{fathersName}</p>
-              </div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-sm-6">
-                <p class="mb-0">Mother's Name</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">{mothersName}</p>
+              <div class="flex-grow-1 ms-3">
+                <h2 class="mb-1">{name}</h2>
+                <p class="mb-2 pb-1" >{username}</p>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Age</p>
+                    <p class="mb-0">{age}</p>
+                  </div>
+                  <div class="px-3">
+                    <p class="small text-muted mb-1">Gender</p>
+                    <p class="mb-0">{sex}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Email</p>
+                    <p class="mb-0">{email}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Contact Number</p>
+                    <p class="mb-0">{contactno}</p>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Father's Name</p>
+                    <p class="mb-0">{fathersName}</p>
+                  </div>
+                  <div class="px-3">
+                    <p class="small text-muted mb-1">Mother's Name</p>
+                    <p class="mb-0">{mothersName}</p>
+                  </div>
+                  </div>
+                  <div class="d-flex justify-content-start rounded-3 p-2 mb-2">
+                  <div>
+                    <p class="small text-muted mb-1">Address</p>
+                    <p class="mb-0">{address}</p>
+                  </div>
+                </div>
+                <div class="d-flex pt-1">
+                  <button type="button" class="btn btn-outline-primary me-1 flex-grow-1">Edit Profile</button>
+                </div>
               </div>
             </div>
           </div>
@@ -105,7 +79,7 @@ export default function ProfileBody() {
       </div>
     </div>
   </div>
-  </div>
-  </div>
+</section>
+</div>
   )
 }
