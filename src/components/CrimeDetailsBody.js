d4b00571a5268ee3a3422a765e14c3f8f7355ba3
Arjun Vinod <arjunvinod09@gmail.com>
2023-08-03 21:07:45 +0530

    new page

---
+++ src/components/CrimeDetailsBody.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CrimeDetailsBody() {
+  return (
+    <div>
+        Crime details here
+    </div>
+  )
+}
---
+++ src/screens/CrimeDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+import CrimeDetailsBody from '../components/CrimeDetailsBody'
+
+export default function CrimeDetails() {
+  return (
+    <div>
+        <Header></Header>
+        <CrimeDetailsBody></CrimeDetailsBody>
+    </div>
+  )
+}
