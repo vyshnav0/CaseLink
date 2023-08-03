1fcb9310ba615a12ab836b12f712490dfb00e85f
Vyshnav <vyshnavk78@gmail.com>
2023-08-03 21:47:25 +0530

    analysis chart: needs review

--- src/screens/Analysis.js
+++ src/screens/Analysis.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import Chart from 'react-apexcharts';
 import Header from '../components/Header'
+import CrimeData from  './CRIME_DATA .json';
 
 export default function Analysis() {
+  function countCrimesByType(data) {
+    const crimeCounts = {};
+    for (const crime of data) {
+      const crimeType = crime.type;
+      if (crimeCounts[crimeType]) {
+        crimeCounts[crimeType]++;
+      } else {
+        crimeCounts[crimeType] = 1;
+      }
+    }
+    return crimeCounts;
+  }
+  const noOfCrimes = countCrimesByType(CrimeData);
+  console.log(noOfCrimes);
+  const categories = ['Trespassing', 'Theft', 'Vandalism', 'Cybercrime', 'Drug Possession', 'Assault', 'Fraud', 'Murder', 'Others'];
+  const data = categories.map((crimeType) => noOfCrimes[crimeType]);
     const series = [
         {
           name: "No. of reported cases", //will be displayed on the y-axis
-          data: [43, 53, 50, 57]
+          // data: [43, 53, 50, 57]
+          data: data
+          
         }
       ];
       const options = {
         chart: {
           id: "caselink_report"
+
         },
         xaxis: {
-          categories: [
-            "Vandalsim",
-            "Robbery",
-            "Murder",
-            "Attempt to Murder"
-          ] //will be displayed on the x-asis
+          title: {
+            text: "Crimes",
+            style: {
+              fontSize: '20px', 
+            }
+          },
+          categories: categories,       
+        },
+        yaxis: {
+          title: {
+            text: "Rate",
+            style: {
+              fontSize: '20px', 
+            }
+          },
         }
       };
   return (
