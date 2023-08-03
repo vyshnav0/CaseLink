04cc595b6ecb83af5b3daa0a1642bdffa5b9f463
Yadukrishna T K <yadutk80@gmail.com>
2023-08-03 13:04:28 +0530

    added cases

--- src/components/LoginBody.js
+++ src/components/LoginBody.js
@@ -78,30 +78,28 @@ export default function LoginBody() {
   }
   
   return (
-    <div className='login'>
-      <form onSubmit={handleSubmit}>
-       
-        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-        <MDBTabsContent>
+  <div class="container">
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <div class='card my-5'>
 
-    {/* <MDBTabsPane show={justifyActive === 'tab1'}> */}
+        <form onSubmit={handleSubmit} class="card-body cardbody-color p-lg-5">
 
-    <MDBInput className='input' wrapperClass='mb-4' label='Email' id='form1' type='text' name = 'email' value = {credentials.email} onChange={onChange}/>
-    <MDBInput className='input' wrapperClass='mb-4' label='Password' id='form2' type='password' name = 'password' value = {credentials.password} onChange={onChange}/>
-
-    <div className="d-flex justify-content-between mx-4 mb-4">
+          <div class="text-center">
+            <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px" alt="profile"/>
+          </div>
+          <MDBInput className='form-control' wrapperClass='mb-3' aria-describedby="emailHelp" placeholder='Email' id='form1' type='text' name = 'email' value = {credentials.email} onChange={onChange}/>
+          <MDBInput className='form-control' wrapperClass='mb-3' placeholder='Password' id='form2' type='password' name = 'password' value = {credentials.password} onChange={onChange}/>
+          <div class="text-center"><button type="submit" class="btn btn-primary me-1 flex-grow-1 w-100 mt-3 mb-5 px-5">Login</button></div>
+          <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not
+            Registered? <a href="/signupcitizen" class="text-dark fw-bold"> Create an
+              Account</a>
+          </div>
+        </form>
+      </div>
+      </div>
     </div>
-    <Button className="mb-4 w-100" type='submit'>Sign in</Button>
-
-    <p className="text-center">Not a member? <a href="/signupcitizen">Register</a></p>
-
-    {/* </MDBTabsPane> */}
-
-    </MDBTabsContent>
-
-    </MDBContainer>
-    </form>  
-    </div>
+  </div>
 
   )
 }
