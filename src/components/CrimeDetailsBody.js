a1781dbbd28b908166749b69eea7a7c5ebcc4606
Arjun Vinod <arjunvinod09@gmail.com>
2023-08-06 12:16:58 +0530

    closed cases also shown in officer crimes

--- src/components/CrimeDetailsBody.js
+++ src/components/CrimeDetailsBody.js
@@ -77,6 +77,9 @@ export default function CrimeDetailsBody() {
             officer : {
               $pull : {
                 opencases : `${cid}`
+              },
+              $push : {
+                closedcases : `${cid}`
               }
             }
           })
