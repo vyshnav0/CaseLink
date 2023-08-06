529fdd83d508ad710dc228f7137ef889186fbdaa
Yadukrishna T K <yadutk80@gmail.com>
2023-08-06 12:24:46 +0530

    }

--- src/components/CrimeDetailsBody.js
+++ src/components/CrimeDetailsBody.js
@@ -242,7 +242,7 @@ export default function CrimeDetailsBody() {
                       </div>
                       </div>
             </div>
-            </div>}
+            </div>
             </p>
             </div>
         </div>
