a9da1ac7d9728a5fadc7836f1e9b33a91ac2dd11
Arjun Vinod <arjunvinod09@gmail.com>
2023-08-06 20:12:18 +0530

    logic changed

--- src/components/CrimeDetailsBody.js
+++ src/components/CrimeDetailsBody.js
@@ -235,7 +235,7 @@ export default function CrimeDetailsBody() {
             </div>
             </div>}
   
-            {!officer &&
+            {(!officer || (officer && closed)) &&
             <div>
               <tr>
                   {/* <td class="text-end">{criminal}</td> */}
